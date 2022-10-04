6613fa14296c1cd3002fd80b1fe3a64dde7eae5c
Link_Rokkie <Dajico12345@gmail.com>
2022-10-04 16:11:24 -0300

    restaurant 1.3

---
+++ app/src/main/java/com/carpetaandroid/prueba1_pesata1/Listas.java
@@ -0,0 +1,14 @@
+package com.carpetaandroid.prueba1_pesata1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listas);
+    }
+}
