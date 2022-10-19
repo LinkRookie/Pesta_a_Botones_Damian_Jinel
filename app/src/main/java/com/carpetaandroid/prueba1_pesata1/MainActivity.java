99c4128ac995d8d72e46b8a8732b519fdc3fc734
Link_Rokkie <Dajico12345@gmail.com>
2022-10-19 20:14:56 -0300

    restaurant 1.5

--- app/src/main/java/com/carpetaandroid/prueba1_pesata1/MainActivity.java
+++ app/src/main/java/com/carpetaandroid/prueba1_pesata1/MainActivity.java
@@ -3,12 +3,29 @@ package com.carpetaandroid.prueba1_pesata1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.botones_direccionales_damian_jinel);
+    }
+    public void menu(View view){
+    Intent intent = new Intent(this,HOME.class);
+    startActivity(intent);
+    }
+    public void lista(View view){
+        Intent intent = new Intent(this,Listas.class);
+        startActivity(intent);
+    }
+    public void perfil(View view){
+        Intent intent = new Intent(this,Perfil.class);
+        startActivity(intent);
+    }
+    public void promo(View view){
+        Intent intent = new Intent(this,Promos.class);
+        startActivity(intent);
     }
 }
